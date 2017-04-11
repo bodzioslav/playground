aab4c253930b6530af725a214e85ec59ec49343d
pwg96 <pwg96pl@gmail.com>
2017-04-12 01:12:40 +0200

    Added some checks

--- cur.py
+++ cur.py
@@ -5,41 +5,44 @@ import requests
 
 def get_currency_rate(currency):
    
-   url = 'http://api.nbp.pl/api/exchangerates/rates/a/{}/?format=json'.format(currency.upper())
-   response = requests.get(url)
+    url = 'http://api.nbp.pl/api/exchangerates/rates/a/{}/?format=json'.format(currency.upper())
+    response = requests.get(url)
 
-   if response.status_code == 200:
-      currency_rate = response.json()['rates'][0]['mid']
-      return currency_rate
-   else:
-      exit("An error occured while fetching currency rates!")
+    try:
+        if response.status_code == 200:
+            currency_rate = response.json()['rates'][0]['mid']
+            return currency_rate
+        else:
+            exit("An error occured while fetching currency rates!")
+    except:
+        exit("Error while trying to fetch the data")
 
 def parse_currency_rate(currency):
    
-   if currency.upper() == 'PLN': 
-      currency_rate = 1
-   else:
-      currency_rate = get_currency_rate(currency)
+    if currency.upper() == 'PLN': 
+        currency_rate = 1
+    else:
+        currency_rate = get_currency_rate(currency)
 
-   return currency_rate
+    return currency_rate
 
 if __name__ == "__main__":
 
-   parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-   parser.add_argument("amount", help="how much you want to convert", type=float)
-   parser.add_argument("source_currency", help="currency you want to convert from")
-   parser.add_argument("target_currency", help="currency you want to convert to")
-   parser.add_argument("-v", "--verbose", help="increase output verbosity", action = "store_true")
+    parser.add_argument("amount", help="how much you want to convert", type=float)
+    parser.add_argument("source_currency", help="currency you want to convert from")
+    parser.add_argument("target_currency", help="currency you want to convert to")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action = "store_true")
 
-   args = parser.parse_args()
+    args = parser.parse_args()
 
-   source_value = parse_currency_rate(args.source_currency)
-   target_value = parse_currency_rate(args.target_currency)
+    source_value = parse_currency_rate(args.source_currency)
+    target_value = parse_currency_rate(args.target_currency)
    
-   result = round(args.amount * source_value / target_value, 2) 
+    result = round(args.amount * source_value / target_value, 2) 
 
-   if args.verbose:
-      print('Converted from {} to {}:'.format(args.source_currency, args.target_currency), result)
-   else:
-      print(result)
+    if args.verbose:
+        print('Converted from {} to {}:'.format(args.source_currency, args.target_currency), result)
+    else:
+        print(result)
